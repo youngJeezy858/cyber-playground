a7d0361e869a469d9fedbcced9aac1f7ee90ff5a
Kyle Frank <kfrank@mix.wvu.edu>
2014-06-10 23:22:58 +0000

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CyberPlayground::Application.config.secret_token = '998a861e8678e6f9b1ddd56bc4b1c6e7ecdaf14d273770ff73c9be82928eb31a86864e0465f4eccc6d48cd7293fa072196aab4227f8d43817de89f2bea50e576'
